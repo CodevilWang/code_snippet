ceb0b473c97518458fcb7a223e14988bfa55b197
codevil <codevil@wang.(none)>
2014-04-06 16:28:29 +0800

    First Blood

---
+++ C++/HelloWorld.cpp
@@ -0,0 +1,23 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2014 Codevil, personal. All Rights Reserved
+ * $Id$ 
+ * 
+ **************************************************************************/
+ 
+ /**
+ * @file HelloWorld.cpp
+ * @author codevil(codevil.w.m@gmail.com)
+ * @date 2014/04/06 16:16:38
+ * @version $Revision$ 
+ * @brief 
+ *  
+ **/
+#include <stdio.h>
+int main()
+{
+    printf("HelloWorld\n");
+    return 0;
+}
+
+/* vim: set ts=4 sw=4 sts=4 tw=100 */
