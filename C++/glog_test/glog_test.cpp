7eb0bec0ac83aa46c21491a8e5b6c4331566e300
CodevilWang <codevil.w.m@gmail.com>
2018-09-08 00:04:58 +0800

    add glog_test & libevent demo

---
+++ C++/glog_test/glog_test.cpp
@@ -0,0 +1,14 @@
+// Copyright 2018 All Rights Reserved.
+// Author: codevil.w.m@gmail.com (M W)
+#include <stdio.h>
+#include <glog/logging.h>
+#include <gflags/gflags.h>
+int main(int argc, char** argv) {
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+    google::InitGoogleLogging(argv[0]);
+    int a = 10;
+    LOG(INFO) << "Found " << a << " apples";
+    return 0;
+}
+
+/* vim: set expandtab ts=2 sw=2 sts=2 tw=80: */
