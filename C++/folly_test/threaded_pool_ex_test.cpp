3774669b97748ddc867cadc275ea46d756cb3c8a
CodevilWang <codevil.w.m@gmail.com>
2018-12-10 23:12:29 +0800

    add threaded_pool_ex_test

---
+++ C++/folly_test/threaded_pool_ex_test.cpp
@@ -0,0 +1,29 @@
+// Copyright 2018 All Rights Reserved.
+// Author: codevil.w.m@gmail.com (M W)
+#include <stdio.h>
+#include <thread>
+#include <chrono>
+#include <folly/executors/ThreadedExecutor.h>
+#include <folly/Function.h>
+void work() {
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    printf("after sleep 10000 ms\n");
+}
+int main() {
+    // work(10);
+    printf("heer\n");
+    // work(1000);
+    printf("heer\n");
+    folly::ThreadedExecutor th_ex;
+    th_ex.add(folly::Function<void()>(work));
+    th_ex.add(folly::Function<void()>(work));
+    th_ex.add(folly::Function<void()>(work));
+    th_ex.add(folly::Function<void()>(work));
+    // th_ex.add(work(3000));
+    // th_ex.add(work(2000));
+    // th_ex.add(work(5000));
+    // th_ex.add(work(4000));
+    return 0;
+}
+
+/* vim: set expandtab ts=2 sw=2 sts=2 tw=80: */
