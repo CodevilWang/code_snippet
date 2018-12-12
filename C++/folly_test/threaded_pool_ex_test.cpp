9f0bf27c38d20769b4acf107cba0317e9419d9e8
CodevilWang <codevil.w.m@gmail.com>
2018-12-12 18:54:12 +0800

    test CPUThreadPool

--- C++/folly_test/threaded_pool_ex_test.cpp
+++ C++/folly_test/threaded_pool_ex_test.cpp
@@ -4,6 +4,7 @@
 #include <thread>
 #include <chrono>
 #include <folly/executors/ThreadedExecutor.h>
+#include <folly/executors/CPUThreadPoolExecutor.h>
 #include <folly/Function.h>
 void work() {
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
@@ -14,7 +15,11 @@ int main() {
     printf("heer\n");
     // work(1000);
     printf("heer\n");
+#ifdef THREADED_EX
     folly::ThreadedExecutor th_ex;
+#elif defined THREAD_POOL_EX
+    folly::CPUThreadPoolExecutor th_ex{4};
+#endif
     th_ex.add(folly::Function<void()>(work));
     th_ex.add(folly::Function<void()>(work));
     th_ex.add(folly::Function<void()>(work));
