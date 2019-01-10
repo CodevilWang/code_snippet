abf7fd960800b0529c3f362752a8ee8ac18e1bb0
CodevilWang <codevil.w.m@gmail.com>
2019-01-10 16:12:13 +0800

    add future_test

---
+++ C++/folly_test/future_test.cpp
@@ -0,0 +1,28 @@
+// Copyright 2018 All Rights Reserved.
+// Author: codevil.w.m@gmail.com (M W)
+#include <stdio.h>
+#include <chrono>
+#include <thread>
+#include <folly/futures/Future.h>
+int main() {
+    folly::Promise<int> p;
+    folly::Future<int> f = p.getFuture();
+    folly::Future<int> ff = std::move(f);
+    std::move(ff).then([] {
+        printf("in future callbacks.\n");
+    });
+    std::thread t([]() ->int {
+        for (int i = 0; i < 10; ++i) {
+            printf("cur %d\n", i);
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        }
+        return 0;
+    });
+    std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+    p.setValue(10);
+    std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+    t.join();
+    return 0;
+}
+
+/* vim: set expandtab ts=2 sw=2 sts=2 tw=80: */
