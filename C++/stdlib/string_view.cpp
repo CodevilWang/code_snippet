d97e50889fe36ce82ec61e75eccc4c9982445e35
CodevilWang <codevil.w.m@gmail.com>
2019-02-25 17:09:56 +0800

    test string_view

---
+++ C++/stdlib/string_view.cpp
@@ -0,0 +1,31 @@
+// Copyright 2018 All Rights Reserved.
+// Author: codevil.w.m@gmail.com (M W)
+#include <stdio.h>
+#include <string_view>
+int main() {
+    std::string_view sv("abc def");
+    printf("%s\n", sv.data());
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* vim: set expandtab ts=2 sw=2 sts=2 tw=80: */
