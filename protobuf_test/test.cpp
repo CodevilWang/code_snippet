0b8e630dca419596d75c0fcb29fdff8028697ea0
CodevilWang <codevil.w.m@gmail.com>
2019-07-30 11:01:09 +0800

    test protobuf case sensentive

--- protobuf_test/test.cpp
+++ protobuf_test/test.cpp
@@ -2,10 +2,18 @@
 // Author: codevil.w.m@gmail.com (M W)
 #include <stdio.h>
 #include <assert.h>
+#ifdef USE_PB3
 #include "AddressBook.pb.h" 
+#else
+#include "AddressBook2.pb.h" 
+#endif
 
 int main() {
+#ifdef USE_PB3
     example::Person p;
+#else
+    example2::Person p;
+#endif
     p.set_name("wm");
     p.set_id(11);
     p.add_pid(12);
@@ -34,11 +42,18 @@ int main() {
     memcpy(data2, p.mutable_pid(), p.pid_size() * sizeof(int32_t));
     printf("%d\n", data2[0]);
     printf("%d\n", data2[1]);
-
+#ifdef USE_PB3
     example::Contact c;
+#else
+    example2::Contact c;
+#endif
     c.set_phone_num("1324555");
     c.set_address("beijing");
+#ifdef USE_PB3
     c.set_ctype(example::ContactType::Home);
+#else
+    c.set_ctype(example2::ContactType::Home);
+#endif
     std::string cstring;
     if (!c.SerializeToString(&cstring)) {
         printf("serialize failed.");
@@ -46,14 +61,22 @@ int main() {
     }
     p.set_contact(cstring);
 
+#ifdef USE_PB3
     example::Contact d;
+#else
+    example2::Contact d;
+#endif
     if (!d.ParseFromString(p.contact())) {
         printf("deserialize failed.\n");
         return 1;
     }
     assert(d.phone_num() == "1324555");
     assert(d.address() == "beijing");
+#ifdef USE_PB3
     assert(d.ctype() == example::ContactType::Home); 
+#else
+    assert(d.ctype() == example2::ContactType::Home); 
+#endif
     printf("%s\n", d.phone_num().c_str());
     printf("%s\n", d.address().c_str());
     return 0;
